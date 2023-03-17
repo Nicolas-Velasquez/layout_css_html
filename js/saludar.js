c11e2661ba68735dad2b7948e696fc71bcdb7abb
Sala D7 <CSalaD7@uniboyaca.edu.co>
2023-03-16 19:22:44 -0500

    Pequeños cambios en saludar.html y saludar.js

--- js/saludar.js
+++ js/saludar.js
@@ -1,10 +1,15 @@
-//document.getElementById("nombre").setAttribute("value", "Johan")
+    document.getElementById("saludarBtn").addEventListener("click", () => {
+        let nombrePersona = document.getElementById("nombre").value
+        if(!nombrePersona)
+            alert("Debe ingresar el nombre")
+        else
+            alert("Hola "+nombrePersona+"!!!!")
+    })
 
-document.getElementById("saludarBtn").addEventListener("click", () => {
-    let nombrePersona = document.getElementById("nombre").value
-    if(!nombrePersona)
-        alert("Debe ingresar el nombre")
-    else
-        alert("Hola "+nombrePersona+"!!!!")
-})
-
+    function saludar(){
+        let nombrePersona = document.getElementById("nombre").value
+        if(!nombrePersona)
+            alert("Debe ingresar el nombre")
+        else
+            alert("Hola "+nombrePersona+"!!!!")
+    }
